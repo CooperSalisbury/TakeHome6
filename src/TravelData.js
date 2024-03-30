19fbbff4d87d2b53e4425b540e7ab4e87cb88396
Cooper Salisbury <csalisbury@oakland.edu>
2024-03-30 14:50:41 -0400

    first commit

--- src/TravelData.js
+++ src/TravelData.js
@@ -3,15 +3,15 @@
 // import Place1Image1 from "./Assets/filename.extension" and similarly for the rest.
 // You can use the images that I have provided or use your own
 // I have provided a lorem ipsum content for description. Just use that. No need to spend time figuring out a description.
-import Place1Image1 from "./assets/LondonImage1.jpg";
-import Place1Image2 from "./assets/LondonImage2.jpg";
-import Place1Image3 from "./assets/LondonImage3.jpg";
-import Place2Image1 from "./assets/SFImage1.jpg";
-import Place2Image2 from "./assets/SFImage2.jpg";
-import Place2Image3 from "./assets/SFImage3.jpg";
-import Place3Image1 from "./assets/TokyoImage1.jpg";
-import Place3Image2 from "./assets/TokyoImage2.jpg";
-import Place3Image3 from "./assets/TokyoImage3.jpg";
+import Place1Image1 from "./assets/TokyoImage1.jpg";
+import Place1Image2 from "./assets/TokyoImage2.jpg";
+import Place1Image3 from "./assets/TokyoImage3.jpg";
+import Place2Image1 from "./assets/LondonImage1.jpg";
+import Place2Image2 from "./assets/LondonImage2.jpg";
+import Place2Image3 from "./assets/LondonImage3.jpg";
+import Place3Image1 from "./assets/SFImage1.jpg";
+import Place3Image2 from "./assets/SFImage2.jpg";
+import Place3Image3 from "./assets/SFImage3.jpg";
 const TravelData = [
   {
     id: 1,
